9621c08a1a2f55601bf4e108b44970ba7d41be97
Ella-Krause <ekrause5@huskers.unl.edu>
2023-09-19 12:37:10 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Eleanor Krause
+ *
+ * date: 9/19/2023
+ *
+ * code that prints your name and major
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("My name is Eleanor Krause\n");
+}
+
